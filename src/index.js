480780f0c1b6f506f30ef9162e2e2d28b640ee96
AssanSav <assane.savadogo81@gmail.com>
2020-02-17 13:29:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
+  var addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -12,4 +12,69 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+  addNewToys();
+  renderAllToys(); 
+  window.onload = addLikesToToys();
 });
+
+function renderAllToys() {
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(respObject => respObject.map(toy => {
+      newDiv = document.createElement("div");
+      newDiv.classList.add("card");
+      newDiv.id = `${toy.id}`;
+      newDiv.innerHTML = `<h2>${toy.name}</h2>
+                            <img src="${toy.image}" class="toy-avatar" />
+                            <p>${toy.likes} Likes </p>
+                          <button class="like-btn">Like <3</button>
+                          `;                      
+      document.querySelector("#toy-collection").appendChild(newDiv);
+  })) 
+}
+
+function addNewToys() {
+  document.querySelector(".submit").addEventListener("click", (event) => {
+    event.preventDefault();
+    let newToyName = event.target.parentElement.name.value;
+    let newToyImage = event.target.parentElement.image.value;
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: newToyName,
+        image: newToyImage
+      })
+    }).then(resp => resp.json())
+      .then(toy => {
+        console.log("t", toy)
+        newDiv = document.createElement("div");
+        newDiv.classList.add("card");
+        newDiv.id = toy.id;
+          newDiv.innerHTML = `<h2>${toy.name}</h2>
+                                <img src="${toy.image}" class="toy-avatar" />
+                                <p>${toy.likes} Likes </p>
+                              <button class="like-btn">Like <3</button>
+                              `;
+        document.querySelector("#toy-collection").appendChild(newDiv);
+    }) 
+  })
+}
+
+
+function addLikesToToys() {
+  let likeBtns = document.querySelectorAll(".like-btn")
+  console.log(likeBtns)
+  for (const like of likeBtns) {
+    let likeParent = like.parentElement
+    debugger
+    like.addEventListener("click", (event) => {
+      let toyLikes = parseInt(event.target.likeParent.querySelector("p").innerHTML.split(" ")[0])
+    })
+  }
+}
+
+  
